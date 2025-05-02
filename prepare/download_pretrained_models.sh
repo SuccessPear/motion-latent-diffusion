eb41805212dd47d946f73bbe1e9105c279f87c07
SuccessPear <lethanhcong3920@gmail.com>
2025-05-02 14:24:19 +0000

    Change pretrain model folder to models

--- prepare/download_pretrained_models.sh
+++ prepare/download_pretrained_models.sh
@@ -1,6 +1,6 @@
-mkdir -p checkpoints/
-cd checkpoints/
-echo -e "The pretrained models will stored in the 'checkpoints' folder\n"
+mkdir -p models/
+cd models/
+echo -e "The pretrained models will stored in the 'models' folder\n"
 mkdir -p mld_humanml3d_checkpoint/
 cd mld_humanml3d_checkpoint/
 # mld_humanml3d.ckpt
